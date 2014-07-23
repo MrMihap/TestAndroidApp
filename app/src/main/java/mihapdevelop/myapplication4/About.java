bdf565a9c86c17d68aec94da9566cfd8e30222a7
MrMihap <MrMihap@yandex.ru>
2014-07-24 02:33:42 +0400

    InitialCommit

---
+++ app/src/main/java/mihapdevelop/myapplication4/About.java
@@ -0,0 +1,19 @@
+package mihapdevelop.myapplication4;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class About extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+}
---
+++ app/src/main/java/mihapdevelop/myapplication4/main.java
@@ -0,0 +1,57 @@
+package mihapdevelop.myapplication4;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.content.Intent;
+
+public class main extends Activity {
+    private TextView mHelloTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public void onClick(View view) {
+        mHelloTextView.setText("Hello Kitty!");
+
+    }
+
+    public void SwitchPanelToCreateTrack(View view) {
+        Intent intent = new Intent(main.this, CreateTrack.class);
+        startActivity(intent);
+
+    }
+
+    public void SwitchPanelToOptions(View view) {
+        Intent intent = new Intent(main.this, Options.class);
+        startActivity(intent);
+    }
+}
